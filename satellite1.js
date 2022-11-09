fd29c387a2220625b53cd7b2ac76f16db512c88f
kilian <kilian.froge@utt.fr>
2022-11-09 14:27:58 +0100

    Correction Implémentation de la certification magenta

--- satellite1.js
+++ satellite1.js
@@ -1,6 +1,7 @@
 var satellite1 = {
     check: function(){
         console.log("Le Satelitte1 est bien dans l'atmosphère");
+        console.log("Magenta level checked".magenta);
         return true;
     }
 }
