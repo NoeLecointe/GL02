9bcf406009c62c7d8f19f59feafd444ec00db0e7
Kinien <kilianfroge@outlook.fr>
2022-11-24 16:23:31 +0100

    ajout fct AddCreneau

--- Salle.js
+++ Salle.js
@@ -1,9 +1,61 @@
 class Salle {
     constructor(nom){
-        this.nomSalle = nom;
+        this.nomSalle = nom,
         this.agenda = Array.apply(null, new Array(7)).map(function(){ return Array.apply(null,new Array(48)); });
-        // voir pour Object.seal(agenda)
+        Object.seal(this.agenda);
     }
 }
 
-export default Salle;
+Salle.prototype.addCreneau = function(jour, debut, fin, creneau){
+    let indiceJour;
+    switch(jour){
+        case "L":
+            indiceJour = 0;
+            break;
+        case "MA":
+            indiceJour = 1;
+            break;
+        case "ME":
+            indiceJour = 2;
+            break;
+        case "J":
+            indiceJour = 3;
+            break;
+        case "V":
+            indiceJour = 4;
+            break;
+        case "S":
+            indiceJour = 5;
+            break;
+        case "D":
+            indiceJour = 6;
+            break;
+        default:
+            indiceJour = -1;
+            break;
+    }
+    let indiceDebut = this.calculIndiceHeure(debut);
+    let indiceFin = this.calculIndiceHeure(fin);
+    for(let i = indiceDebut; i<= indiceDebut; i++){
+        this.agenda[indiceJour][i] = creneau;
+    }
+       
+    
+
+
+}
+
+Salle.prototype.calculIndiceHeure = function(heure){
+    let indiceheure;
+    const tabHeureMinute = heure.split(":");
+    indiceheure = Number(tabHeureMinute[0]) * 2;
+    if(Number(tabHeureMinute[1]) >= 30){
+        indiceheure++;
+    }
+    return indiceheure;
+}
+
+
+    
+
+
