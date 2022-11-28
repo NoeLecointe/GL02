6b8d66b6e51878278b27ad2571e5eeb2a73a7250
Tony Gong <tony.gong@utt.fr>
2022-11-28 19:15:49 +0100

    spec 2 code mis au propre avec la gestion d'erreur

--- actions.js
+++ actions.js
@@ -2,12 +2,7 @@ const fs = require('fs');
 const parserUeSalle = require('./parserUeSalle.js');
 class Actions{
     static actionUeSalle = function({logger, args}){
-        logger.info("The list of rooms of %s is", args.ue);
         let pathdata;
-        //if (String(args.ue).substring(0,1)==="A" || String(args.ue).substring(0,1)==="B"){
-        //    pathdata = "AB";
-        //};
-
         const firstLetter = String(args.ue).substring(0,1);
         switch(firstLetter){
             case "A": case "B":
@@ -44,27 +39,27 @@ class Actions{
         fs.readFile('./SujetA_data/'+pathdata+'/edt.cru','utf8', function (err,data){
 
             if (err){
-                return logger.warn(err);
+                return logger.warn("UE not found or not exist, please write the ue name with uppercase");
             }
+            const expressionue = /[A-Z]{2,10}[0-9]{0,2}[A-Z]{0,1}[0-9]{0,1}/;
+            if (String(args.ue).match(expressionue)){
             var analyzer = new parserUeSalle();
             analyzer.parse(data,String(args.ue));
-            //const ueselected = analyzer.selectue(String(args.ue));
-            //const expressionsalle = new RegExp("S=[A-Z][0-9]{3}/g");
             const expressionsalle = /S=[A-Z][0-9]{3}|S=[A-Z]{3}[0-9]|S=[A-Z]{4}/g;
+            if (typeof analyzer.uesearched === 'undefined'){ 
+                console.log("UE not found on the data base");
+            } else {
             let listesalle = analyzer.uesearched.match(expressionsalle);
-            //for(let salle of listesalle){
-            //    console.log(salle);
-            //};
             var listesalleunique = new Set();
             listesalle.forEach(element => {
                 listesalleunique.add(element.substring(2));
             });
+            logger.info("The list of rooms of %s is", args.ue);
             console.log(listesalleunique);
-            //console.log(listesalle);
-            //console.log(listesalle[0]);
-            //console.log(analyzer.listroom[1]);
-            //console.log(data);
-           //a venir
+            }
+        } else {
+            console.log("UE not found because of wrong syntax for the name of the UE");
+        }
         })
     }
 }
--- parserUeSalle.js
+++ parserUeSalle.js
@@ -1,26 +1,20 @@
 var parser = function(){
     this.uesearched;
-    this.symb = ["+","P=","H=","S=","//"];
 };
-
-
 
 parser.prototype.parse = function(data, uename){
 	var tData = this.getlistue(data);
 	var ue = tData.filter((val) => val.match(uename));
+	//Si uename contient 2 lettres seulement (ex : BI), et qu'une autre ue contient ces 2 lettres ( ex : BI01), 
+	//cela va match et afficher les salles de BI01 alors que BI n'existe pas mais BI est syntaxement correct.
+	//  match.(uename+'$') ne marche pas ni ${uename}$ pourtant dans la BNF nom d'ue est suivi d'un CRLF mais ces deux cas ne trouvent aucun match après
 	this.uesearched = ue[0];
 };
 
 parser.prototype.getlistue = function(data){
 	var separator = /\+/;
-	data = data.split(separator);
-	//data = data.filter((val) => !val.match(separator)); 					
+	data = data.split(separator);					
 	return data;
 };
-
-//parser.prototype.selectue = function(uename){
-//	var ue = this.listroom.filter((val) => val.match(uename)); 
-//	return ue[0];
-//};
 
 module.exports = parser;
