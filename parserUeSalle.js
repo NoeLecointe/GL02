b1911f9c80617a4437978e4a94a89dfdc036e7a9
Tony Gong <tony.gong@utt.fr>
2022-11-29 23:51:32 +0100

    Ajout du parser et command pour la spec 3

--- parserUeSalle.js
+++ parserUeSalle.js
@@ -1,18 +1,24 @@
 var parser = function(){
-    this.uesearched;
+    this.searched;
 };
 
-parser.prototype.parse = function(data, uename){
-	var tData = this.getlistue(data);
-	var ue = tData.filter((val) => val.match(uename));
-	//Si uename contient 2 lettres seulement (ex : BI), et qu'une autre ue contient ces 2 lettres ( ex : BI01), 
-	//cela va match et afficher les salles de BI01 alors que BI n'existe pas mais BI est syntaxement correct.
-	//  match.(uename+'$') ne marche pas ni ${uename}$ pourtant dans la BNF nom d'ue est suivi d'un CRLF mais ces deux cas ne trouvent aucun match après
-	this.uesearched = ue[0];
+parser.prototype.parse = function(data, name, spec){
+	var tData = this.getlist(data, spec);
+	var target = tData.filter((val) => val.match(name));
+	if (spec === 2){
+		this.searched = target[0];
+	} else {
+		this.searched = target;
+	}
 };
 
-parser.prototype.getlistue = function(data){
-	var separator = /\+/;
+parser.prototype.getlist = function(data, spec){
+	var separator;
+	if (spec === 2){
+		separator = /\+/;
+	} else {
+		separator = /\/\//;
+	}
 	data = data.split(separator);					
 	return data;
 };
